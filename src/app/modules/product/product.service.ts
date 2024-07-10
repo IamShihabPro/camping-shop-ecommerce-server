a183a2eed95f597c52a1b804d555cdf17c97b2c8
IamShihabPro <mshihab.pro@gmail.com>
2024-07-10 11:33:03 +0600

    product create

---
+++ src/app/modules/product/product.service.ts
@@ -0,0 +1,29 @@
+import QueryBuilder from "../../builder/QueryBuilder"
+import { TProduct } from "./product.interface"
+import { Product } from "./product.model"
+
+const createProductsIntoDB = async(payload: TProduct)=>{
+    const result = await Product.create(payload)
+    return result
+}
+
+const getAllProductsFromDB = async (query: Record<string, unknown>) => {
+    const productSearchableFields = ['name', 'category', 'tags'];
+    const productsQuery = new QueryBuilder(
+        Product.find(),
+      query,
+    )
+      .search(productSearchableFields)
+      .filter()
+      .sort()
+      .paginate()
+      .fields();
+  
+    const result = await productsQuery.modelQuery;
+    return result;
+};
+
+export const ProductsServices ={
+    createProductsIntoDB,
+    getAllProductsFromDB,
+}
